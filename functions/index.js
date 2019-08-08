e2f3b324ad8cc4c47799373d3366b3cd90540165
jantony1 <jantony789@gmail.com>
2019-08-07 22:55:46 -0400

    Added Angular

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,14 @@ const functions = require("firebase-functions");
 const express = require("express");
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
+const _app_folder = './qrclient/dist';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send(`${Date.now()}`);
+app.get('*', express.static('./qrclient/dist', {maxAge: '1y'}));
+
+app.all('*', function (req, res) {
+    res.status(200).sendFile(`/`, {root:  _app_folder});
 });
 
 exports.app = functions.https.onRequest(app);
