c6288dd06c45472f5210d091c8a8ce6854cd2bf9
jantony1 <jantony789@gmail.com>
2019-08-07 22:09:56 -0400

    timestamp

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,12 @@
 const functions = require("firebase-functions");
 const express = require("express");
-const engines = require("consolidate");
-const firebase = require("firebase-admin");
-
-const firebaseApp = firebase.initializeApp(functions.config().firebase);
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
 const app = express();
-app.engine("hbs", engines.handlebars);
-app.set("views", "./views");
-app.set("view engine", "hbs");
 
 app.get("/", (request, response) => {
-  facts = ["1: Hello world", "2: Do you hear me"];
-  response.render("facts", { facts });
+  response.send(`${Date.now()}`);
 });
 
 exports.app = functions.https.onRequest(app);
