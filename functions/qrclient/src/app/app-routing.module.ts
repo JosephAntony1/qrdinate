2422eaf5ec28948cc38b5091c0b39cb34aa58c80
jantony1 <jantony789@gmail.com>
2019-08-09 19:03:59 -0400

    Two components now

--- functions/qrclient/src/app/app-routing.module.ts
+++ functions/qrclient/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import Homepage from "./hompage/compage.component";
+import EventInfo from "./event-info/event-info.component";
+const routes: Routes = [{ path: "home", component: Homepage},  {path: 'event-info' , component: EventInfo} ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- functions/qrclient/src/app/app.module.ts
+++ functions/qrclient/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { EventInfoComponent } from './event-info/event-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    EventInfoComponent
   ],
   imports: [
     BrowserModule,
